2d6e9cc8d02060eba4ecf6190e32879c8b6f2a7a
Souvik Ghosh <57757570+S907@users.noreply.github.com>
2023-03-01 10:28:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import PdfViewer from './PdfViewer';
 
 function App() {
-  return <PdfViewer clientId="e72f8162d91e455d98e6105d92446480" />;
+  return <PdfViewer clientId="<clientId>" />;
 }
 
 export default App;
